bbcf4ca683910388501fda18d6cf846517360367
Evergreen brain PVT. LTD <69231498+hemraj-php@users.noreply.github.com>
2023-09-22 23:33:39 +0530

    First run commit

--- src/contentScript/ContentScript.tsx
+++ src/contentScript/ContentScript.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react"
 
 export const ContentScript: FunctionComponent = () => {
+    console.log('it run');
     return (
         <div className="absolute top-24 left-0 right-0 flex items-center justify-center">
             <div className="bg-white px-8 py-4 rounded-lg shadow-xl text-gray-800">
